45f943944ec2038c64f8893f2a06f6b308a9bea7
David MOSCA <david.mosca69@gmail.com>
2020-12-20 11:22:50 +0100

    initial commit parcel, babel, postcss posthtml, react

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const hello = "david";
+console.log("coucou" + hello);
+ReactDOM.render(<App />, document.getElementById("root"));
